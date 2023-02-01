d7d2935be2ed46c75451f0c071ab7de563bd6469
hjr0125 <hanjr0125@gmail.com>
2023-02-01 17:43:40 +0900

    update

--- crawling_json.R
+++ crawling_json.R
@@ -25,8 +25,28 @@ response <- httr::POST(url, body = payload, encode = "form")
 json_data <- jsonlite::fromJSON(content(response, as = "text"))
 
 # extract the gridExportData from the JSON object
-grid_data <- json_data$gridExportData
-grid_foot <- json_data$gridExportFootData
+grid_data <- json_data$gridImportData
+grid_foot <- json_data$gridImportFootData
 
 grid_df_foot <- fromJSON(grid_foot)
+#전체 소계
+#FTA_INCRE_RATIO -> BASE_YEAR 증감률
+#FTA_INCRE_RATIO1 -> BASE_YEAR - 1 증감률
+#FTA_INCRE_RATIO2 -> BASE_YEAR - 2 증감률
+#FTA_SUM_DLR -> BASE_YEAR 금액
+#FTA_SUM_DLR1 -> BASE_YEAR - 1 금액
+#FTA_SUM_DLR2 -> BASE_YEAR - 2 금액
+#TARIFF_DLR -> BASE_YEAR 수혜금액
+#TARIFF_DLR1 -> BASE_YEAR -1 수혜금액
+#TARIFF_DLR2 -> BASE_YEAR -2 수혜금액
+
+
 grid_df_data <- fromJSON(grid_data)
+#IM_AMT_OF_DLR -> BASE_YEAR 금액
+#PRE_IM_AMT_OF_DLR1 -> BASE_YEAR - 1 금액
+#PRE_IM_AMT_OF_DLR2 -> BASE_YEAR - 2 금액
+#TARIFF_DLR -> BASE_YEAR 특혜 적용 금액
+#TARIFF_DLR1 -> BASE_YEAR - 1 특혜 적용 금액
+#TARIFF_DLR2 -> BASE_YEAR - 2 특혜 적용 금액
+#NATN_NAME -> 국가
+#FTANAME -> FTA
